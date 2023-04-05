206baf8c9b9400875ac265b0ccc85765a25bd413
leonardo <a20201696@pucp.edu.pe>
2023-04-04 19:52:43 -0500

    Initial commit

---
+++ src/test/java/com/example/lab2_2/Lab22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
